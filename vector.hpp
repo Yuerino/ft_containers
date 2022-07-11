2c5f47346455d2d994bf89a0f7be917a8ea97eeb
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-07-12 00:35:29 +0200

    chore: indent

--- vector.hpp
+++ vector.hpp
@@ -15,18 +15,18 @@ namespace ft {
 	template< typename T, typename Allocator = std::allocator<T> >
 	class vector {
 	public:
-		typedef T																	value_type;
-		typedef Allocator															allocator_type;
-		typedef typename Allocator::reference										reference;
-		typedef typename Allocator::const_reference									const_reference;
-		typedef typename Allocator::pointer											pointer;
-		typedef typename Allocator::const_pointer									const_pointer;
-		typedef ft::random_access_iterator<pointer, vector>							iterator;
-		typedef ft::random_access_iterator<const_pointer, vector>					const_iterator;
-		typedef ft::reverse_iterator<iterator>										reverse_iterator;
-		typedef ft::reverse_iterator<const_iterator>								const_reverse_iterator;
-		typedef std::ptrdiff_t														difference_type;
-		typedef std::size_t															size_type;
+		typedef T													value_type;
+		typedef Allocator											allocator_type;
+		typedef typename Allocator::reference						reference;
+		typedef typename Allocator::const_reference					const_reference;
+		typedef typename Allocator::pointer							pointer;
+		typedef typename Allocator::const_pointer					const_pointer;
+		typedef ft::random_access_iterator<pointer, vector>			iterator;
+		typedef ft::random_access_iterator<const_pointer, vector>	const_iterator;
+		typedef ft::reverse_iterator<iterator>						reverse_iterator;
+		typedef ft::reverse_iterator<const_iterator>				const_reverse_iterator;
+		typedef std::ptrdiff_t										difference_type;
+		typedef std::size_t											size_type;
 
 	public:
 		// construct/copy/destroy
