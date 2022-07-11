fa48f4de9e4f285edf9ee0cf115d74d2e84bd7aa
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-07-11 15:48:04 +0200

    feat: add pair and make_pair

--- utility.hpp
+++ utility.hpp
@@ -1,10 +1,72 @@
 #pragma once
 
 namespace ft {
+	// ft::swap
 	template<typename T>
 	void swap(T& a, T& b) {
 		T tmp = a;
 		a = b;
 		b = tmp;
+	}
+
+	// ft::pair
+	template<typename T1, typename T2>
+	struct pair {
+		typedef T1	first_type;
+		typedef T2	second_type;
+
+		first_type	first;
+		second_type	second;
+
+		pair() : first(), second() {}
+
+		template<class U, class V>
+		pair(const pair<U, V>& pr) : first(pr.first), second(pr.second) {}
+
+		pair(const first_type& a, const second_type& b) : first(a), second(b) {};
+
+		pair& operator=(const pair& pr) {
+			this->first = pr.first;
+			this->second = pr.second;
+			return *this;
+		}
+
+		~pair() {}
+	};
+
+	template<typename T1, typename T2>
+	bool operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		 return (lhs.first == rhs.first) && (lhs.second == rhs.second);
+	}
+
+	template<typename T1, typename T2>
+	bool operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return !(lhs == rhs);
+	}
+
+	template<typename T1, typename T2>
+	bool operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return (lhs.first < rhs.first) || (!(rhs.first < lhs.first) && lhs.second < rhs.second);
+	}
+
+	template<typename T1, typename T2>
+	bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return !(rhs < lhs);
+	}
+
+	template<typename T1, typename T2>
+	bool operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return rhs < lhs;
+	}
+
+	template<typename T1, typename T2>
+	bool operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs) {
+		return !(lhs < rhs);
+	}
+
+	// ft::make_pair()
+	template<typename T1, typename T2>
+	pair<T1, T2> make_pair(T1 x, T2 y) {
+		return pair<T1, T2>(x, y);
 	}
 }
